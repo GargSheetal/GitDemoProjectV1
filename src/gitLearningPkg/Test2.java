afdbcc89b8f77a8dd71d3afdbb08aea52d246c0e
sheetalgargqa <kanhakuapoojan@gmail.com>
2022-09-29 23:37:25 -0400

    Added Test2

---
+++ src/gitLearningPkg/Test2.java
@@ -0,0 +1,21 @@
+package gitLearningPkg;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		String hungry = "Yes";
+		
+		if(hungry == "Yes") 
+		{
+			System.out.println("Eat something......");
+		}
+		else
+		{
+			System.out.println("Keep Working...");
+		}
+
+
+	}
+
+}
